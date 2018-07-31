1694d2de543ce99babe14f6befbb4ee441e274b6
daliDeli <bruns8900@gmail.com>
2018-08-01 01:23:21 +0200

    changes made to gitignore

--- src/utils/constants.js
+++
@@ -1,3 +0,0 @@
-export const BASE_URL = 'https://api.github.com/';
-export const AUTH_TOKEN = '197ac6bac1e25ab7124c661b79ae51b88440d5ca';
-export const DEFAULT_IMG = 'http://www.hope3k.net/images/placeholders/team-placeholder.jpg';
